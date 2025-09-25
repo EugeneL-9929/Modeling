37af48ca694d69f8bd1c505bd41327dd691ca531
EugeneL-9929 <yliujz@connect.ust.hk>
2025-09-25 16:26:52 +0800

    change at 1626 20250925

---
+++ TextProcessor/DynamicProcessor.py
@@ -0,0 +1,8 @@
+# import model from hugging face
+import os
+# os.system('python -m venv .env')
+# os.system('.env/Scripts/activate')
+# os.system('conda install transformers')
+import torch.nn as nn
+import torch
+from transformers import pipeline
--- TextProcessor/VectorProcessor.py
+++ TextProcessor/VectorProcessor.py
@@ -1,9 +1,11 @@
 from gensim.models import Word2Vec
-# CBOW - continuous bag of words (input: )
-# SG - skip gram 
+# CBOW - continuous bag of words (input: context output: center)
+# SG - skip gram (input: center output: context) 
 import gensim.downloader as api
 # loading pre-trained word2vec model
 # wv = api.load('word2vec-google-news-300')
+from gensim.models import KeyedVectors
+# model = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
 # t-distrbuted stochastic neighbour embedding
